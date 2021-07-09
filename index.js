6ef0b9a55083573dd5321f567e968acbacce2328
Shaeyi-lu <57910040+Shaeyi-lu@users.noreply.github.com>
2021-07-08 23:48:14 -0400

    firebase

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
- // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyDsBBBeYKwcOuAl4ZJ1Q_5-lef7wOPP_vk",
-    authDomain: "ctieapp.firebaseapp.com",
-    projectId: "ctieapp",
-    storageBucket: "ctieapp.appspot.com",
-    messagingSenderId: "872354465781",
-    appId: "1:872354465781:web:614531615204992e5542bc",
-    measurementId: "G-YEFB8Y0XD0"
-  };
 
-  
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBM75V8Mm881ufS4hCxtSnkUus3uNWYkg4",
+    authDomain: "form-204e2.firebaseapp.com",
+    projectId: "form-204e2",
+    storageBucket: "form-204e2.appspot.com",
+    messagingSenderId: "506325767761",
+    appId: "1:506325767761:web:c2eed99dfc60bdcf3a8a70"
+  };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   
 
   const auth = firebase.auth();
 
-function signUp(){
+  function signUp(){
     var email = document.getElementById("email");
     var pass = document.getElementById("pass");
 
-    const promise = auth.createUserWithEmailAndPassword(email.value,pass.value);
+    const promise = auth.createUserWithEmailAndPassword(email.value, pass.value);
+    
 
-    promise.catch(e=>alert(e.message));
+    promise.catch(e => alert(e.message));
     alert("Signed Up");
     
 }
